ce9b4b52c7238f9c7897f20ad3c0b786a8723918
Carl Ortutay <carlorty@gmail.com>
2019-07-21 10:56:55 -0400

    updated questionStore.js with questions & answers

---
+++ questionStore.js
@@ -0,0 +1,63 @@
+const STORE = [
+  {
+    question: "How many Star Wars ‘Episodes” are there? (pre Disney)",
+    answers: [2, 4, 6, 8],
+    correctAnswer: "2"
+  },
+  {
+    question: "Of those ‘Episodes’ which is the first released?",
+    answers: [2, 4, 6, 8],
+    correctAnswer: "4"
+  },
+  {
+    question: "Which (pre Disney) 'Episode' was the last to be released?",
+    answers: [2, 4, 6, 8],
+    correctAnswer: "6"
+  },
+  {
+    question: "Which ‘Episode’ does Lando Calrissian first appear?",
+    answers: [1, 3, 5, 7],
+    correctAnswer: "5"
+  },
+  {
+    question: "Who is Luke Skywalker’s father?",
+    answers: ["Han Solo", "Ben Kenobi", "Darth Vader", "Admiral Ackbar"],
+    correctAnswer: "Darth Vader"
+  },
+  {
+    question: `“That’s no moon. It’s a space station.” What is this quote in reference too?`,
+    answers: [
+      "Star Destroyer",
+      "Death Star",
+      "Super Star Destroyer",
+      "Millenium Falcon"
+    ],
+    correctAnswer: "Death Star"
+  },
+  {
+    question: "What planet was destroyed in ‘A New Hope’?",
+    answers: ["Coruscant", "Tatooine", "Endor", "Alderaan"],
+    correctAnswer: "Alderaan"
+  },
+  {
+    question: "Which character made the order to destroy the planet?",
+    answers: [
+      "Darth Vader",
+      "Grand Moff Tarkin",
+      "Chewbacca",
+      "Admiral Thrawn"
+    ],
+    correctAnswer: "Grand Moff Tarkin"
+  },
+  {
+    question:
+      "What was the name of the bounty hunter who took Han Solo to Jabba the Hutt?",
+    answers: ["Boba Fett", "Lando Calrissian", "Nein Nunb", "C-3PO"],
+    correctAnswer: "Boba Fett"
+  },
+  {
+    question: "When Lea tell Han she loves him, what was his response?",
+    answers: ["I love you, too", "Shut up", "I told you so", "I know"],
+    correctAnswer: "I know"
+  }
+];
