a1ece226e33b5bee534c5e7755671121ad184284
Carl Ortutay <carlorty@gmail.com>
2019-07-21 13:32:07 -0400

    updated script.js

---
+++ script.js
@@ -0,0 +1,79 @@
+let qNum = 0;
+let score = 0;
+
+function questSpawn () {
+  if (qNum < STORE.length) {
+    return `<div class="question-${qNum}">
+    <h2>${STORE[qNum].question}</h2>
+    <form>
+    <fieldset>
+    <label class="selectAns">
+    <input type="radio" value="${STORE[qNum].answers[0]}" name="answer" required>
+    <span>${STORE[qNum].answers[0]}</span>
+    </label>
+    <label class="selectAns">
+    <input type="radio" value="${STORE[qNum].answers[1]}" name="answer" required>
+    <span>${STORE[qNum].answers[1]}</span>
+    </label>
+    <label class="selectAns">
+    <input type="radio" value="${STORE[qNum].answers[2]}" name="answer" required>
+    <span>${STORE[qNum].answers[2]}</span>
+    </label>
+    <label class="selectAns">
+    <input type="radio" value="${STORE[qNum].answers[3]}" name="answer" required>
+    <span>${STORE[qNum].answers[3]}</span>
+    </label>
+    <button type="submit" class="submitButton">Submit</button>
+    </fieldset>
+    </form>
+    </div>`;
+  } else {
+    renderResults();
+    restartQuiz();
+    $('.qNum').text(10)
+  }
+}
+
+function updateQuestNum() {
+  qNum++;
+  $(".qNum").text(qNum + 1);
+}
+
+function updateScore() {
+  score++;
+}
+
+function startQuiz() {
+  $(".startPage").on("click", ".startButton", function(event) {
+    $(".startPage").remove();
+    $(".showQuest").css("display", "block");
+    $(".qNum").text(1);
+    renderQuest();
+  });
+}
+
+function renderQuest() {
+  $(".showQuest").html(questSpawn());
+}
+
+function userAnswer() {
+  $("form").on("submit", function(event) {
+    event.preventDefault();
+    let selected = $("input:checked");
+    let answer = selected.val();
+    let correctAnswer = `${STORE[qNum].correctAnswer}`;
+    if (answer === correctAnswer) {
+      selected.parent().addClass("correct");
+      ifCorrect();
+    } else {
+      selected.parent().addClass("wrong");
+      ifWrong();
+    }
+  });
+}
+
+function generateQuiz() {
+  startQuiz();
+}
+
+$(generateQuiz);
