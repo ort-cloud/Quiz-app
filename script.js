9f79611b5ca57ac07962ceac003b745ef0a47c5a
Carl Ortutay <carlorty@gmail.com>
2019-07-21 20:26:09 -0400

    made minor functionality adjustments. almost ready to merge

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ function questSpawn () {
     <form>
     <fieldset>
     <label class="selectAns">
+    <p>You may fire when ready.</p>
     <input type="radio" value="${STORE[qNum].answers[0]}" name="answer" required>
     <span>${STORE[qNum].answers[0]}</span>
     </label>
@@ -23,7 +24,7 @@ function questSpawn () {
     <input type="radio" value="${STORE[qNum].answers[3]}" name="answer" required>
     <span>${STORE[qNum].answers[3]}</span>
     </label>
-    <button type="submit" class="submitButton">submit</button>
+    <button type="submit" class="submitButton">FIRE!</button>
     </fieldset>
     </form>
     </div>`;
@@ -48,7 +49,6 @@ function startQuiz() {
     $(".startPage").remove();
     $(".showQuest").css("display", "block");
     $(".qNum").text(1);
-    //renderQuest();
   });
 }
 
@@ -97,9 +97,15 @@ function newScore(){
   $('.score').text(score);
 }
 
-function usersResults(){
-  //write if/else statement here
-}
+function userResults(){
+  if (score >= 8) {
+    $('.showQuest').html(`<div class="results correctFeedback"><h2>Good. Gooood!</h2><img src="https://banner2.kisspng.com/20180412/eiq/kisspng-star-wars-battlefront-ii-palpatine-anakin-skywalke-darth-vader-5ad00eb85594f7.1932252115235846963506.jpg" alt="Emperor Palpatine"/><p>You got ${score} / 10</p><p>I can feel the force flowing through you!</p><button class="restartButton">Restart Quiz</button></div>`);
+  } else if (score < 8 && score >= 5) {
+    $('.showQuest').html(`<div class="results correctFeedback"><h2>Do or do not. There is no try.</h2><img src="https://banner2.kisspng.com/20180202/lzq/kisspng-yoda-darth-maul-star-wars-jedi-star-wars-transparent-background-5a7487326a2b16.7780970115175862264349.jpg" alt="Master Yoda"/><p>You got ${score} / 10</p><p>Not bad, but not good either.</p><button class="restartButton">Restart Quiz</button></div>`);
+  } else {
+    $('.showQuest').html(`<div class="results correctFeedback"><h2>I find your lack of knowledge disturbing</h2><img src="https://banner2.kisspng.com/20180329/vhq/kisspng-star-wars-the-force-unleashed-ii-anakin-skywalker-darth-vader-5abd0ea6990b60.5714685715223394946269.jpg" alt="Darth Vader"/><p>You got ${score} / 10</p><p>Do not under estimate the power of the force.</p><button class="restartButton">Restart Quiz</button></div>`);
+  }
+  }
 
 function nextQuest(){
   $('main').on('click','.nextButton', function (event){
@@ -110,7 +116,9 @@ function nextQuest(){
 }
 
 function restartQuiz(){
- //Write reload here
+ $('main').on('click', '.restartButton', function(event){
+    location.reload();
+ })
 }
 
 function generateQuiz() {
