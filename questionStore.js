45db0188e74cba6b896394a46675b7944f9102a6
Carl Ortutay <carlorty@gmail.com>
2020-02-19 19:26:41 -0500

    Replaced dead img links with hard assets. Some styling adjustments

--- questionStore.js
+++ questionStore.js
@@ -1,47 +1,49 @@
+
+
 const STORE = [
   {
     question: "How many Star Wars ‘Episodes” are there? (pre Disney)",
     answers: [2, 4, 6, 8],
     correctAnswer: "6",
-    correctImg: 'http://www.sclance.com/clipart/han-solo-clip-art/han-solo-clip-art_964559.png',
+    correctImg: './assets/han-solo.png',
     altCorrect: 'Han Solo',
-    wrongImg: 'https://tchol.org/images/admiral-ackbar-png-9.png',
+    wrongImg: './assets/admiral-ackbar.png',
     altWrong: 'Admiral Ackbar'
   },
   {
     question: "Of those ‘Episodes’ which was the first released?",
     answers: [2, 4, 6, 8],
     correctAnswer: "4",
-    correctImg: 'http://www.sclance.com/clipart/han-solo-clip-art/han-solo-clip-art_964559.png',
+    correctImg: './assets/han-solo.png',
     altCorrect: 'Han Solo',
-    wrongImg: 'https://tchol.org/images/admiral-ackbar-png-9.png',
+    wrongImg: './assets/admiral-ackbar.png',
     altWrong: 'Admiral Ackbar'
   },
   {
     question: "Which (pre Disney) 'Episode' was the last to be released?",
     answers: [2, 4, 6, 8],
     correctAnswer: "6",
-    correctImg: 'http://www.sclance.com/clipart/han-solo-clip-art/han-solo-clip-art_964559.png',
+    correctImg: './assets/han-solo.png',
     altCorrect: 'Han Solo',
-    wrongImg: 'https://tchol.org/images/admiral-ackbar-png-9.png',
+    wrongImg: './assets/admiral-ackbar.png',
     altWrong: 'Admiral Ackbar'
   },
   {
     question: "Which ‘Episode’ does Lando Calrissian first appear?",
     answers: [1, 3, 5, 7],
     correctAnswer: "5",
-    correctImg: 'http://www.sclance.com/clipart/han-solo-clip-art/han-solo-clip-art_964559.png',
+    correctImg: './assets/han-solo.png',
     altCorrect: 'Han Solo',
-    wrongImg: 'https://tchol.org/images/admiral-ackbar-png-9.png',
+    wrongImg: './assets/admiral-ackbar.png',
     altWrong: 'Admiral Ackbar'
   },
   {
     question: "Who is Luke Skywalker’s father?",
     answers: ["Han Solo", "Ben Kenobi", "Darth Vader", "Admiral Ackbar"],
     correctAnswer: "Darth Vader",
-    correctImg: 'http://www.sclance.com/clipart/han-solo-clip-art/han-solo-clip-art_964559.png',
+    correctImg: './assets/han-solo.png',
     altCorrect: 'Han Solo',
-    wrongImg: 'https://tchol.org/images/admiral-ackbar-png-9.png',
+    wrongImg: './assets/admiral-ackbar.png',
     altWrong: 'Admiral Ackbar'
   },
   {
@@ -53,18 +55,18 @@ const STORE = [
       "Millenium Falcon"
     ],
     correctAnswer: "Death Star",
-    correctImg: 'http://www.sclance.com/clipart/han-solo-clip-art/han-solo-clip-art_964559.png',
+    correctImg: './assets/han-solo.png',
     altCorrect: 'Han Solo',
-    wrongImg: 'https://tchol.org/images/admiral-ackbar-png-9.png',
+    wrongImg: './assets/admiral-ackbar.png',
     altWrong: 'Admiral Ackbar'
   },
   {
     question: "What planet was destroyed in ‘A New Hope’?",
     answers: ["Coruscant", "Tatooine", "Endor", "Alderaan"],
     correctAnswer: "Alderaan",
-    correctImg: 'http://www.sclance.com/clipart/han-solo-clip-art/han-solo-clip-art_964559.png',
+    correctImg: './assets/han-solo.png',
     altCorrect: 'Han Solo',
-    wrongImg: 'https://tchol.org/images/admiral-ackbar-png-9.png',
+    wrongImg: './assets/admiral-ackbar.png',
     altWrong: 'Admiral Ackbar'
   },
   {
@@ -76,9 +78,9 @@ const STORE = [
       "Grand Admiral Thrawn"
     ],
     correctAnswer: "Grand Moff Tarkin",
-    correctImg: 'http://www.sclance.com/clipart/han-solo-clip-art/han-solo-clip-art_964559.png',
+    correctImg: './assets/han-solo.png',
     altCorrect: 'Han Solo',
-    wrongImg: 'https://tchol.org/images/admiral-ackbar-png-9.png',
+    wrongImg: './assets/admiral-ackbar.png',
     altWrong: 'Admiral Ackbar'
   },
   {
@@ -86,18 +88,18 @@ const STORE = [
       "What was the name of the bounty hunter who took Han Solo to Jabba the Hutt?",
     answers: ["Boba Fett", "Lando Calrissian", "Nein Nunb", "Bossk"],
     correctAnswer: "Boba Fett",
-    correctImg: 'http://www.sclance.com/clipart/han-solo-clip-art/han-solo-clip-art_964559.png',
+    correctImg: './assets/han-solo.png',
     altCorrect: 'Han Solo',
-    wrongImg: 'https://tchol.org/images/admiral-ackbar-png-9.png',
+    wrongImg: './assets/admiral-ackbar.png',
     altWrong: 'Admiral Ackbar'
   },
   {
     question: "When Lea tells Han she loves him, what was his response?",
     answers: ["I love you, too", "Shut up", "I told you so", "I know"],
     correctAnswer: "I know",
-    correctImg: 'http://www.sclance.com/clipart/han-solo-clip-art/han-solo-clip-art_964559.png',
+    correctImg: './assets/han-solo.png',
     altCorrect: 'Han Solo',
-    wrongImg: 'https://tchol.org/images/admiral-ackbar-png-9.png',
+    wrongImg: './assets/admiral-ackbar.png',
     altWrong: 'Admiral Ackbar'
   }
 ];
